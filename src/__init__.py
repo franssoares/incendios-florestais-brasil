16a068bb34982aacd6e7b63dc1f658519d3c1782
franssoares <xflsn100@gmail.com>
2025-06-27 01:35:27 -0300

    sync

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,4 @@
 # src/__init__.py
-# Pode estar vazio ou definir imports básicos se quiser
-# Por exemplo, para facilitar importar o preprocess direto: isso é como um catalogo de funcoes dessa pasta que podem ser usadas em outras pastas
+#para facilitar importar o preprocess direto: isso é como um catalogo de funcoes dessa pasta que podem ser usadas em outras pastas
 from .preprocess import *
 
